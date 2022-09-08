01a0b2a5c9df90c40ac3663092b04d5c1202f0cc
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-09-08 00:00:30 -0300

    problem solved

--- elixir/freelancer-rates/freelancer.ex
+++ elixir/freelancer-rates/freelancer.ex
@@ -10,21 +10,28 @@ defmodule FreelancerRates do
 
   def monthly_rate(hourly_rate, discount_percentage) do
     total = daily_rate(hourly_rate) * 22
-    calculate_percentage(total, discount_percentage)
+
+    round_and_trunc(total)
+    |> apply_discount(discount_percentage)
+    |> round_and_trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount_percentage) do
-    amount_with_discount =
-      montly_rate(hourly_rate)
+    daily_with_discount =
+      daily_rate(hourly_rate)
       |> apply_discount(discount_percentage)
 
+    budget / daily_with_discount
+    |> Float.floor(1)
   end
 
   defp calculate_percentage(amount, percentage) do
     (percentage / 100) * amount
   end
 
-  defp montly_rate(hourly_rate) do
-    daily_rate(hourly_rate) * 22
+  defp round_and_trunc(value) do
+    value
+    |> Float.ceil
+    |> trunc
   end
 end
