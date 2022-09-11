99b7f7f9014160168f9daf32d22bcedfa12db036
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-09-10 23:16:52 -0300

    add exercise body

--- elixir/pacman_rules/lib/pacman_rules.ex
+++ elixir/pacman_rules/lib/pacman_rules.ex
@@ -1,18 +1,17 @@
-defmodule PacmanRules do
-  @moduledoc """
-  Documentation for `PacmanRules`.
-  """
+defmodule Rules do
+  def eat_ghost?(power_pellet_active, touching_ghost) do
+    # Please implement the eat_ghost?/2 function
+  end
 
-  @doc """
-  Hello world.
+  def score?(touching_power_pellet, touching_dot) do
+    # Please implement the score?/2 function
+  end
 
-  ## Examples
+  def lose?(power_pellet_active, touching_ghost) do
+    # Please implement the lose?/2 function
+  end
 
-      iex> PacmanRules.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
+    # Please implement the win?/3 function
   end
 end
